2162af13508f14116c9f72b3f72f1a8d40875dbb
Immanuel Mruk <immanuel.mruk@8mylez.com>
2024-10-19 16:33:33 +0200

    Refactor stuff

--- app.py
+++ app.py
@@ -8,12 +8,10 @@ load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
 
-
 @app.after_request
 def add_header(response):
     response.headers['X-Robots-Tag'] = 'noindex, nofollow'
     return response
-
 
 @app.route('/robots.txt')
 def robots_txt():
@@ -23,16 +21,13 @@ def robots_txt():
         app.logger.error(f"Error generating robots.txt: {str(e)}")
         return Response("Error generating robots.txt", status=500)    
 
-
 @app.route('/camera')
 def camera():
     return render_template('camera.html')
 
-
 @app.route('/camera-feed')
 def camera_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
 
 def generate_frames():
     camera = cv2.VideoCapture(0)
@@ -47,6 +42,5 @@ def generate_frames():
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT')), debug=True)
