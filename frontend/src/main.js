a49ac65acabc600efb088747ab04725c26bb9ff3
Immanuel Mruk <immanuelmruk@gmail.com>
2024-10-27 22:20:35 +0100

    Remove dashboard for now

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,11 @@
 import { createApp } from "vue"
 import { createRouter, createWebHistory } from "vue-router"
 import App from "./App.vue"
-import DashboardPage from "./pages/dashboard-page.vue"
 import AdminPage from "./pages/admin-page.vue"
 import CameraPage from "./pages/camera-page.vue"
 
 const routes = [
-  { path: "/", name: "Dashboard", component: DashboardPage },
-  { path: "/admin", name: "Admin", component: AdminPage },
+  { path: "/", name: "Admin", component: AdminPage },
   { path: "/camera", name: "Camera", component: CameraPage },
 ];
 
