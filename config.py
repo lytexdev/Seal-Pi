2dfab38e223eefde490100ee7fa99219f7b22175
Immanuel Mruk <immanuelmruk@gmail.com>
2024-10-27 13:37:12 +0100

    Clean up

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('FLASK_SECRET_KEY', '!@?SuperSecretKey?@!')
-    PORT = int(os.getenv('PORT', 8080))
+    PORT = int(os.getenv('PORT', 5000))
     VIDEO_CAPTURE_DEVICE = int(os.getenv('VIDEO_CAPTURE_DEVICE', 0))
